03fa4920bcfeb6f5e85a7329d832cc3455a54b51
SimonasGer <159446055+SimonasGer@users.noreply.github.com>
2024-08-31 18:52:50 +0300

    ass

--- app.js
+++ app.js
@@ -7,15 +7,6 @@ app.use(express.json());
 
 const cors = require("cors");
 app.use(cors());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://reapirmanfrontend.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-      return res.status(200).end();
-    }
-    next();
-  });
 
 app.use("/users", userRoutes)
 app.use("/repairman", repairmanRouter)
