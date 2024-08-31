3d28edd9ea8a276bd9eaad64e154a4ad71c9022e
SimonasGer <159446055+SimonasGer@users.noreply.github.com>
2024-08-31 18:04:52 +0300

    ass

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const repairmanRouter = require("./routes/repairmanRoutes")
 
 app.use(express.json());
+
+const cors = require("cors");
 app.use(cors());
 
 app.use("/users", userRoutes)
